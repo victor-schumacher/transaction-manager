85051c1f39963de0ee59d2571dc4c71d6e87dd97
Victor <victorschumacher255@gmail.com>
2021-05-27 20:25:29 -0300

    account handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,15 @@
 package main
 
-func main(){
+import (
+	"github.com/labstack/echo"
+	"transaction-manager/http/handler"
+)
 
+func main() {
+	e := echo.New()
+	h := handler.NewAccount()
+	h.MakeAccountHandlers(e)
+	if err := e.Start(":8080"); err != nil {
+		return
+	}
 }
-
---
+++ http/handler/account.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"github.com/labstack/echo"
+	"net/http"
+)
+
+type Account struct {
+}
+
+func NewAccount() Account {
+	return Account{}
+}
+
+func (a Account) findById(c echo.Context) error {
+	return c.JSON(http.StatusOK, `{"account_id":1, "document_number":"09365523859"}`)
+}
+
+func (a Account) createNew(c echo.Context) error {
+	return 	c.JSON(http.StatusCreated, `{"account_id":"1"}`)
+}
+
+func (a Account) MakeAccountHandlers(e *echo.Echo) {
+	e.GET("/accounts", a.findById)
+	e.POST("accounts/", a.createNew)
+}
