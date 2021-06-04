91ab32b46c286df45d72f90f49d3cea0cd96d846
Victor <victorschumacher255@gmail.com>
2021-06-04 08:38:01 -0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # transaction-manager
+### Este repositório é baseado em um teste
+
+### Run
+
+`docker-compose up`
+na raiz do projeto irá subir o banco e o server
+
+é necessário rodar o .sql em `resources/database/`
+
+
--- http/handler/account_test.go
+++ http/handler/account_test.go
@@ -14,47 +14,39 @@ const accountJSON = `{"account_id":"1fe89750-676e-47ca-949d-f97e56111a02", "docu
 
 func TestCreateAccount(t *testing.T) {
 	e := echo.New()
-	req := httptest.NewRequest(http.MethodPost, "/accounts", strings.NewReader(accountJSON))
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	h := NewAccount(mock.AccountRepo{}, e)
+
 
 	table := []struct {
-		name   string
-		body   string
-		method string
+		name         string
+		body         string
+		method       string
+		responseBody string
+		status       int
+		wantErr      bool
 	}{
 		{
-			name:   "success case",
-			body:   accountJSON,
-			method: http.MethodPost,
+			name:         "success case",
+			body:         accountJSON,
+			method:       http.MethodPost,
+			responseBody: "{\"message\":\"account successfully created\"}\n",
+			status:       http.StatusCreated,
+			wantErr:      false,
 		},
 	}
 
 	for _, tt := range table {
 		t.Run(tt.name, func(t *testing.T) {
-			if assert.NoError(t, h.createNew(c)) {
-				assert.Equal(t, http.StatusCreated, rec.Code)
-				assert.Equal(t, "{\"message\":\"account successfully created\"}\n", rec.Body.String())
+			req := httptest.NewRequest(tt.method, "/accounts", strings.NewReader(tt.body))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+			h := NewAccount(mock.AccountRepo{}, e)
+			if !tt.wantErr {
+				assert.NoError(t, h.createNew(c))
 			}
+			assert.Equal(t, tt.status, rec.Code)
+			assert.Equal(t, tt.responseBody, rec.Body.String())
 		})
 	}
 
-
-}
-
-func TestFindOneAccount(t *testing.T) {
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodGet, "/accounts/1", nil)
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	h := NewAccount(mock.AccountRepo{}, e)
-
-	// Assertions
-	if assert.NoError(t, h.findById(c)) {
-		assert.Equal(t, http.StatusOK, rec.Code)
-		assert.Equal(t, "{\"account_id\":\"1\"}\n", rec.Body.String())
-	}
 }
